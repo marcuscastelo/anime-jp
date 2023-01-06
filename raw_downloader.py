ad27086490337522c237b04e117e5f9dde98b33b
marcuscastelo <marcuscastelo@usp.br>
2023-01-06 02:05:22 -0300

    Custom message if qbittorrent is not oppened

--- raw_downloader.py
+++ raw_downloader.py
@@ -2,8 +2,15 @@ from qbittorrent import Client
 
 class RawDownloader:
     def __init__(self) -> None:
-        self._client = Client('http://localhost:8080/', verify=False)
-        pass
+        try:
+            self._client = Client('http://localhost:8080/', verify=False)
+        except Exception as e:
+            # Print original error message
+            print(e)
+
+            # Print custom error message
+            print('Could not connect to qBittorrent. Is it running?')
+            exit(1)
 
     def download(self, magnet: str, dest: str = '.'):
         self._client.download_from_link(magnet, savepath=dest)
